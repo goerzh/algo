826777c4c456b84443cd004263bd7ba0122b911d
goerzh <zhpan188@gmail.com>
2020-02-21 11:42:18 +0800

    Binary Search

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,10 @@
 #![feature(box_into_raw_non_null)]
 #![feature(slice_from_raw_parts)]
+#![feature(core_intrinsics)]
 pub mod vec;
 pub mod list;
 pub mod consist_hash;
 pub mod part_consist_hash;
 pub mod recursive;
 pub mod sort;
+pub mod binary_search;
